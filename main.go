94364eebd5ad68ea585c7c4a1cd5af5624d910f0
ann <ann@yandex.ru>
2025-09-25 18:59:17 +0300

    first commit

--- main.go
+++ main.go
@@ -1,3 +1,34 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	req, err := http.NewRequest("GET", "http://srv.msk01.gigacorp.local/_stats", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// Читаем всё тело ответа
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	// Выводим статус и тело
+	fmt.Println("Статус:", resp.Status)
+	// fmt.Println("Тело ответа:")
+	fmt.Println(string(body))
+}
